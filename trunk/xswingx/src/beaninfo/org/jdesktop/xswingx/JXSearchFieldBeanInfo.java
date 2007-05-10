6b6fa38622931a2c5385deb248f22b82d530fae1
petw2815 <petw2815@abaf64d8-c52d-0410-ab24-9d07384f4a97>
2007-05-10 09:47:10 +0000

    updated beaninfo

--- trunk/xswingx/src/beaninfo/org/jdesktop/xswingx/JXSearchFieldBeanInfo.java
+++ trunk/xswingx/src/beaninfo/org/jdesktop/xswingx/JXSearchFieldBeanInfo.java
@@ -3,6 +3,7 @@ package org.jdesktop.xswingx;
 import org.jdesktop.swingx.EnumerationValue;
 import org.jdesktop.xswingx.JXSearchField;
 import org.jdesktop.xswingx.JXSearchField.LayoutStyle;
+import org.jdesktop.xswingx.JXSearchField.SearchMode;
 
 public class JXSearchFieldBeanInfo extends JXPromptBeanInfo {
 	public JXSearchFieldBeanInfo() {
@@ -11,13 +12,21 @@ public class JXSearchFieldBeanInfo extends JXPromptBeanInfo {
 
 	protected void initialize() {
 		super.initialize();
-		setPreferred(true, "layoutStyle");
-		
+		setPreferred(true, "layoutStyle", "searchMode", "instantSearchDelay", "searchPopupMenu");
+
 		setEnumerationValues(
 				new EnumerationValue[] {
 						new EnumerationValue("Mac", LayoutStyle.MAC,
 								"org.jdesktop.xswingx.JXSearchField.LayoutStyle.MAC"),
 						new EnumerationValue("Vista", LayoutStyle.VISTA,
-								"org.jdesktop.xswingx.JXSearchField.LayoutStyle.VISTA") }, "layoutStyle");
+								"org.jdesktop.xswingx.JXSearchField.LayoutStyle.VISTA") },
+				"layoutStyle");
+		setEnumerationValues(
+				new EnumerationValue[] {
+						new EnumerationValue("Instant", SearchMode.INSTANT,
+								"org.jdesktop.xswingx.JXSearchField.SearchMode.INSTANT"),
+						new EnumerationValue("Regular", SearchMode.REGULAR,
+								"org.jdesktop.xswingx.JXSearchField.SearchMode.REGULAR") },
+				"searchMode");
 	}
 }
