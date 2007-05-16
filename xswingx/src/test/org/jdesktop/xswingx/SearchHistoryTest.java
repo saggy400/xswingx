9ede8d6fb18ef708dcbe0cf27e690a827ff7b1ec
petw2815 <petw2815@abaf64d8-c52d-0410-ab24-9d07384f4a97>
2007-05-16 11:43:54 +0000

    don't save duplicates

--- xswingx/src/test/org/jdesktop/xswingx/SearchHistoryTest.java
+++ xswingx/src/test/org/jdesktop/xswingx/SearchHistoryTest.java
@@ -1,7 +1,6 @@
 package org.jdesktop.xswingx;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertSame;
 
 import java.util.prefs.Preferences;
@@ -25,9 +24,9 @@ public class SearchHistoryTest {
 		sh.removeAll();
 		
 		assertSame(0, sh.getLength());
-		sh.add("search1");
+		sh.put("search1");
 		assertSame(1, sh.getLength());
-		sh.add("search2");
+		sh.put("search2");
 		
 		sh = new SearchHistory("search.test");
 		assertSame(2, sh.getLength());
@@ -36,15 +35,22 @@ public class SearchHistoryTest {
 	@Test
 	public void testRecentsOrder() throws Exception {
 		searchHistory.setMaxRecents(2);
-		searchHistory.add("search1");
-		searchHistory.add("search2");
+		searchHistory.put("search1");
+		searchHistory.put("search2");
 		assertEquals("search2", searchHistory.getRecentSearches()[0]);
 		assertEquals("search1", searchHistory.getRecentSearches()[1]);
 		
-		searchHistory.add("search3");
+		searchHistory.put("search3");
 		assertSame(2, searchHistory.getLength());
 		assertEquals("search3", searchHistory.getRecentSearches()[0]);
 		assertEquals("search2", searchHistory.getRecentSearches()[1]);
+	}
+	
+	@Test
+	public void testDoubleAdd() throws Exception {
+		searchHistory.put("search1");
+		searchHistory.put("search1");
+		assertSame(1, searchHistory.getLength());
 	}
 	
 	@Test
@@ -52,8 +58,8 @@ public class SearchHistoryTest {
 		assertSame(5, searchHistory.getMaxRecents());
 		searchHistory.setMaxRecents(1);
 		assertSame(1, searchHistory.getMaxRecents());
-		searchHistory.add("search1");
-		searchHistory.add("search2");
+		searchHistory.put("search1");
+		searchHistory.put("search2");
 		assertSame(1, searchHistory.getLength());
 	}
 	
