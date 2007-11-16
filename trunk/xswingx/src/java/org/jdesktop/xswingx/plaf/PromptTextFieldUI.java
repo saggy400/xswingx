ae1a5ecc9ae19e23f5062a33b0f0828fca2e5987
petw2815 <petw2815@abaf64d8-c52d-0410-ab24-9d07384f4a97>
2007-11-16 19:02:14 +0000

    fixed mac border problem with buddies

--- trunk/xswingx/src/java/org/jdesktop/xswingx/plaf/PromptTextFieldUI.java
+++ trunk/xswingx/src/java/org/jdesktop/xswingx/plaf/PromptTextFieldUI.java
@@ -1,13 +1,9 @@
 package org.jdesktop.xswingx.plaf;
 
-import java.awt.Graphics;
-
-import javax.swing.JComponent;
 import javax.swing.JTextField;
 import javax.swing.plaf.TextUI;
 import javax.swing.text.JTextComponent;
 
-import org.jdesktop.xswingx.BuddySupport;
 import org.jdesktop.xswingx.NativeSearchFieldSupport;
 
 /**
@@ -50,31 +46,34 @@ public class PromptTextFieldUI extends PromptTextUI {
 		// http://developer.apple.com/technotes/tn2007/tn2196.html#JTEXTFIELD_VARIANT
 		NativeSearchFieldSupport.setSearchField(lbl, NativeSearchFieldSupport.isSearchField(txtField));
 		NativeSearchFieldSupport.setSearchPopupMenu(lbl, NativeSearchFieldSupport.getSearchPopupMenu(txtField));
+		
+		//here we need to copy the border again for Mac OS X, because the above calls may have replaced it.
+		lbl.setBorder(txtField.getBorder());
 
-		// buddy support
-		BuddySupport.setOuterMargin(lbl, BuddySupport.getOuterMargin(txtField));
-		BuddySupport.setLeft(lbl, BuddySupport.getLeft(txtField));
-		BuddySupport.setRight(lbl, BuddySupport.getRight(txtField));
+		// buddy support: not needed, because BuddyLayoutAndBorder queries original text field
+//		BuddySupport.setOuterMargin(lbl, BuddySupport.getOuterMargin(txtField));
+//		BuddySupport.setLeft(lbl, BuddySupport.getLeft(txtField));
+//		BuddySupport.setRight(lbl, BuddySupport.getRight(txtField));
 
 		return lbl;
 	}
  
-//	/**
-//	 * Overriden to support native search fields in Mac OS 10.5.
-//	 * 
-//	 * Also paints the prompt if the text field is a search field as returned by
-//	 * {@link SearchFieldSupport#isSearchField(JTextField)} and native search
-//	 * fields are supported.
-//	 * 
-//	 * @see SearchFieldSupport#isNativeSearchFieldSupported()
-//	 * @see SearchFieldSupport#isSearchField(JTextField)
-//	 */
+	/**
+	 * Overriden to support native search fields in Mac OS 10.5.
+	 * 
+	 * Also paints the prompt if the text field is a search field as returned by
+	 * {@link SearchFieldSupport#isSearchField(JTextField)} and native search
+	 * fields are supported.
+	 * 
+	 * @see SearchFieldSupport#isNativeSearchFieldSupported()
+	 * @see SearchFieldSupport#isSearchField(JTextField)
+	 */
 //	@Override
 //	public void paint(Graphics g, JComponent c) {
 //		JTextField txt = (JTextField) c;
 //
 //		// Paint Mac OS 10.5 native search field.
-//		if (SearchFieldSupport.isNativeSearchField(txt)) {
+//		if (NativeSearchFieldSupport.isNativeSearchField(txt)) {
 //			if (shouldPaintPrompt(txt))
 //				paintPromptComponent(g, txt);
 //			if (!shouldPaintPrompt(txt)) {
