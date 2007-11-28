4080d96bdfe413acada685b084082ad73548049e
petw2815 <petw2815@abaf64d8-c52d-0410-ab24-9d07384f4a97>
2007-11-28 13:46:28 +0000

    ui change handler: prevent accidential double installation

--- xswingx/src/java/org/jdesktop/xswingx/plaf/AbstractUIChangeHandler.java
+++ xswingx/src/java/org/jdesktop/xswingx/plaf/AbstractUIChangeHandler.java
@@ -5,11 +5,20 @@ import java.beans.PropertyChangeListener;
 import javax.swing.JComponent;
 
 public abstract class AbstractUIChangeHandler implements PropertyChangeListener {
+	//prevent double installation
+	private boolean installed;
+	
 	public void install(JComponent c){
+		if(installed){
+			return;
+		}
+		
 		c.addPropertyChangeListener("UI", this);
+		installed = true;
 	}
 	
 	public void uninstall(JComponent c){
 		c.removePropertyChangeListener("UI", this);
+		installed = false;
 	}
 }
